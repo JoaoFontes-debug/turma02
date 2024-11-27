9a2695a1abcafc8798a17f330a49de267699bfe7
João Fontes <j.henry.kings@gmail.com>
2024-11-27 08:25:43 -0300

    starting using swiper, adding adjustment of the images

---
+++ LPC_mario/js/app.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', () => {
+    let swiper = new Swiper('.swiper', {
+        direction:'horiziontal', 
+        loop:true, 
+        pagination: { 
+            el: '.swiper-pagination'
+        },
+
+        navigation:{
+            nextEl:'swiper-button-next', 
+            prevEl:'.swiper-button-prev'
+        }
+    })
+});
+
+document.addEventListener('keydown', (event) => {
+    if(event.key === 'ArrowLeft'){
+        swiper.slidePrev();
+    }else if(event.key === 'ArrowRight'){
+        swiper.slideNext();
+    }
+
+});
